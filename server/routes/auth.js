daba3d4a9c28de12a0417fbbc017e28500875f76
Akshay <akshaymangutkar951@gmail.com>
2025-02-01 20:47:23 +0530

    added login api and done the verification of user in db

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -30,6 +31,39 @@ router.post("/register", async (req, res) => {
     return res
       .status(500)
       .json({ success: false, message: "Error in Adding User" });
+  }
+});
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "user not exist" });
+    }
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Wrong Credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, "secreatkeyofnoteapp123@#", {
+      expiresIn: "5h",
+    });
+    return res.status(200).json({
+      success: true,
+      token,
+      user: { name: user.name },
+      message: "Login Successfully",
+    });
+  } catch (error) {
+    console.log("error", error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Error in Login Server" });
   }
 });
 
