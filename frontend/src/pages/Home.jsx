52be2d24ea4927d05200f421a37ab64255235321
Akshay <akshaymangutkar951@gmail.com>
2025-02-02 13:07:23 +0530

    created modal for adding note

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
+import NoteModal from "../components/NoteModal";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [isModalOpen, setModelOpen] = useState(false);
+  const navigate = useNavigate();
+  const closeModel = () => {
+    setModelOpen(false);
+  };
+  const addNote = async ({ title, description }) => {
+    try {
+      const response = await axios.post("http://localhost:5000/api/note/add", {
+        title,
+        description,
+      });
+      if (response.data.success) {
+        navigate("/");
+        closeModel();
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen">
       <Navbar />
+
+      <button
+        onClick={() => setModelOpen(true)}
+        className="fixed right-4 bottom-4 bg-teal-500 text-white font-bold p-4 rounded-full text-3xl"
+      >
+        +
+      </button>
+      {isModalOpen && <NoteModal closeModel={closeModel} addNote={addNote} />}
     </div>
   );
 };
